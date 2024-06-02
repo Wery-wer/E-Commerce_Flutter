738c92f3f15d6ba639861df8285a967d361a8c24
Wery <44234831+Wery-wer@users.noreply.github.com>
2024-06-02 18:31:21 +0700

    slicing splash

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:shamo_apps/themes/themes.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 3), () => Navigator.pushNamed(context, '/sign-in'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor1,
+      body: Center(
+          child: Container(
+        width: 130,
+        height: 150,
+        decoration: BoxDecoration(
+          image: const DecorationImage(
+            image: AssetImage('assets/image_splash.png'),
+          ),
+        ),
+      )),
+    );
+  }
+}
